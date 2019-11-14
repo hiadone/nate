3991369713b5b6a8a66bfc3b1d8f497c88bdb45a
boyoon8 <boyoon8@gmail.com>
2019-11-14 14:48:38 +0900

    b

--- views/media/b-a-1/1/index.php
+++ views/media/b-a-1/1/index.php
@@ -4,7 +4,8 @@
             'skin' => 'bootstrap',
             'brd_key' => element('brd_key', $view),
             'post_id' => element('post_id', $view),
-            
+            'cache_minute' => 0,
+
         );
       
            echo $this->board->media_iframe($config);
